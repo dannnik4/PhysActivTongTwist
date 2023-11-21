16eed6c4a48af72984a85c40106d0b811f6f52ba
dannnik4 <daniilperstnev2001@gmail.com>
2023-11-21 23:22:44 +0200

    Updated MainActivity.java

--- app/src/main/java/com/example/physactivtongtwist/MainActivity.java
+++ app/src/main/java/com/example/physactivtongtwist/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.physactivtongtwist;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.tab_one:
+                        fragment = new PhysActivTab();
+                        break;
+                    case R.id.tab_two:
+                        fragment = new TongTwistTab();
+                        break;
+                    default:
+                        return false;
+                }
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, fragment);
+                transaction.commit();
+                return true;
+            }
+        });
+
+        // Set the initial fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, new PhysActivTab());
+        transaction.commit();
     }
 }
