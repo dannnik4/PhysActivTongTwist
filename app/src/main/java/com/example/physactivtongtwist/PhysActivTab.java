a5250bd11705db8dcbeb4f514595dc60d86fc196
dannnik4 <daniilperstnev2001@gmail.com>
2023-11-22 23:54:04 +0200

    Added PhysActivTab.java

---
+++ app/src/main/java/com/example/physactivtongtwist/PhysActivTab.java
@@ -0,0 +1,34 @@
+package com.example.physactivtongtwist;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class PhysActivTab extends Fragment {
+
+    public PhysActivTab() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.physactiv_layout, container, false);
+
+        TextView textView = view.findViewById(R.id.WidgetText);
+        Button createWidgetButton = view.findViewById(R.id.WidgetButton);
+
+        createWidgetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle the button click, e.g., create a new widget
+                // You can add your logic here
+            }
+        });
+
+        return view;
+    }
+}
