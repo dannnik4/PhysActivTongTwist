a6e012433c51b921f820b2adad123aca52dd2642
dannnik4 <daniilperstnev2001@gmail.com>
2023-12-08 21:55:16 +0200

    Updated PhysActivTab.java

--- app/src/main/java/com/example/physactivtongtwist/PhysActivTab.java
+++ app/src/main/java/com/example/physactivtongtwist/PhysActivTab.java
@@ -1,14 +1,13 @@
 package com.example.physactivtongtwist;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import androidx.appcompat.app.AlertDialog;
 
 public class PhysActivTab extends Fragment {
 
@@ -26,9 +25,20 @@ public class PhysActivTab extends Fragment {
         createWidgetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Handle the button click, e.g., open a dialog
+                showWidgetDialog();
             }
         });
 
         return view;
+    }
+
+    private void showWidgetDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        builder.setTitle("New Widget")
+                .setMessage("This is your new widget!")
+                .setPositiveButton("OK", null) // You can add a positive button action here
+                .setNegativeButton("Cancel", null) // You can add a negative button action here
+                .show();
     }
 }
