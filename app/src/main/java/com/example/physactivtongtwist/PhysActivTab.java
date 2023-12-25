b98280d08715d95df91a6544a0cbd7d10e93b00e
dannnik4 <daniilperstnev2001@gmail.com>
2023-12-25 22:53:47 +0200

    Updated PhysActivTab.java

--- app/src/main/java/com/example/physactivtongtwist/PhysActivTab.java
+++ app/src/main/java/com/example/physactivtongtwist/PhysActivTab.java
@@ -1,15 +1,17 @@
 package com.example.physactivtongtwist;
 
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
 
 public class PhysActivTab extends Fragment {
+
+    private LinearLayout widgetContainer;
 
     public PhysActivTab() {
     }
@@ -19,16 +21,27 @@ public class PhysActivTab extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.physactiv_layout, container, false);
 
-        TextView textView = view.findViewById(R.id.WidgetText);
+//        widgetContainer = view.findViewById(R.id.widgetContainer);
+
         Button createWidgetButton = view.findViewById(R.id.WidgetButton);
 
         createWidgetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ((MainActivity) requireActivity()).showWidgetDialog();
+
             }
         });
 
         return view;
+    }
+
+    private void addWidget(String widgetText) {
+        // Создать новую запись и добавить ее куда-то на экран
+        View widgetView = LayoutInflater.from(requireContext()).inflate(R.layout.widget_item, null);
+        TextView itemText = widgetView.findViewById(R.id.itemText);
+        itemText.setText(widgetText);
+
+        // Добавить виджет в контейнер
+        widgetContainer.addView(widgetView);
     }
 }
