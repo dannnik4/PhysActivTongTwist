65614010f5b08301bef6f66e5db12173cba6a58f
dannnik4 <daniilperstnev2001@gmail.com>
2024-01-23 23:29:07 +0200

    Updated PhysActivTab.java

--- app/src/main/java/com/example/physactivtongtwist/PhysActivTab.java
+++ app/src/main/java/com/example/physactivtongtwist/PhysActivTab.java
@@ -1,19 +1,12 @@
 package com.example.physactivtongtwist;
 
-import static android.content.Context.MODE_PRIVATE;
-
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import androidx.fragment.app.Fragment;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class PhysActivTab extends Fragment {
 
