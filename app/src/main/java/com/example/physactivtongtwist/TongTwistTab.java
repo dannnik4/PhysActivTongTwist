8c5908d471dacf50b7544a979e8ed4650a9e3086
dannnik4 <daniilperstnev2001@gmail.com>
2024-02-14 22:56:29 +0200

    Updated TongTwistTab.java

--- app/src/main/java/com/example/physactivtongtwist/TongTwistTab.java
+++ app/src/main/java/com/example/physactivtongtwist/TongTwistTab.java
@@ -1,13 +1,9 @@
 package com.example.physactivtongtwist;
 
-import static android.content.Context.MODE_PRIVATE;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 
 public class TongTwistTab extends Fragment {
