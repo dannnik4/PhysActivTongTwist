35df159cd0a3b497d2c11311958a771875c46af7
dannnik4 <daniilperstnev2001@gmail.com>
2024-02-17 23:47:38 +0200

    Updated PhysActivTab.java

--- app/src/main/java/com/example/physactivtongtwist/PhysActivTab.java
+++ app/src/main/java/com/example/physactivtongtwist/PhysActivTab.java
@@ -8,10 +8,10 @@ import androidx.fragment.app.Fragment;
 
 public class PhysActivTab extends Fragment {
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.physactiv_layout, container, false);
-        return view;
-    }
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        View view = inflater.inflate(R.layout.physactiv_layout, container, false);
+//        return view;
+//    }
 }
