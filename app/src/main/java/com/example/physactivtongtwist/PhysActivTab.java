dce7b48c69f5e6bfe171413bbd17f9cb7cae7555
dannnik4 <daniilperstnev2001@gmail.com>
2024-02-18 18:14:22 +0200

    Updated PhysActivTab.java

--- app/src/main/java/com/example/physactivtongtwist/PhysActivTab.java
+++ app/src/main/java/com/example/physactivtongtwist/PhysActivTab.java
@@ -1,17 +1,47 @@
 package com.example.physactivtongtwist;
 
+import static android.content.Context.MODE_PRIVATE;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 
 public class PhysActivTab extends Fragment {
+
+    private TextView textView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.physactiv_layout, container, false);
+
+        textView = view.findViewById(R.id.WidgetText);
+
+        loadWidgetText("PhysActivTab");
+
+        Button createWidgetButton = view.findViewById(R.id.WidgetButton);
+        createWidgetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((MainActivity) requireActivity()).showWidgetDialog(new WidgetDialogCallback() {
+                    @Override
+                    public void onPositiveClick(String widgetText) {
+                        textView.setText(widgetText);
+                    }
+                }, "PhysActivTab");
+            }
+        });
+
         return view;
+    }
+
+    private void loadWidgetText(String tabIndex) {
+        SharedPreferences preferences = requireActivity().getSharedPreferences("MyPreferences", MODE_PRIVATE);
+        String savedText = preferences.getString("widgetText_" + tabIndex, "");
+        textView.setText(savedText);
     }
 }
