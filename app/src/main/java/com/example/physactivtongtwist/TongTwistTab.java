24f0914437bf884092ed1a35e8fb117a4d58e3ff
dannnik4 <daniilperstnev2001@gmail.com>
2024-03-28 18:45:49 +0200

    Updated TongTwistTab.java

--- app/src/main/java/com/example/physactivtongtwist/TongTwistTab.java
+++ app/src/main/java/com/example/physactivtongtwist/TongTwistTab.java
@@ -1,6 +1,8 @@
 package com.example.physactivtongtwist;
 
 import static android.content.Context.MODE_PRIVATE;
+
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -13,6 +15,8 @@ import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.regex.Pattern;
 
 public class TongTwistTab extends Fragment {
@@ -91,5 +95,27 @@ public class TongTwistTab extends Fragment {
 
         // Сохраняем текст в файл
         saveToFile(text);
+    }
+
+    private void saveToFile(String text) {
+        FileOutputStream fos = null;
+        try {
+            // Открываем файл для записи
+            fos = requireContext().openFileOutput("blocks.txt", Context.MODE_APPEND);
+            // Записываем текст в файл
+            fos.write(text.getBytes());
+            // Добавляем символ новой строки для разделения блоков текста
+            fos.write(System.getProperty("line.separator").getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
