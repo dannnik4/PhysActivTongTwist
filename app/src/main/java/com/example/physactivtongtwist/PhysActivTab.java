a2d07c636070e4098a2c8630d978203147140b08
dannnik4 <daniilperstnev2001@gmail.com>
2024-04-19 23:22:58 +0300

    Updated PhysActivTab.java

--- app/src/main/java/com/example/physactivtongtwist/PhysActivTab.java
+++ app/src/main/java/com/example/physactivtongtwist/PhysActivTab.java
@@ -35,7 +35,7 @@ public class PhysActivTab extends Fragment {
                 ((MainActivity) requireActivity()).showWidgetDialog(new WidgetDialogCallback() {
                     @Override
                     public void onPositiveClick(String widgetText) {
-                        addWidget(widgetText);
+                        showWidget(widgetText);
                     }
                 }, "PhysActivTab");
             }
@@ -86,7 +86,7 @@ public class PhysActivTab extends Fragment {
         }
     }
 
-    private void addWidget(String text) {
+    private void showWidget(String text) {
         View blockView = LayoutInflater.from(requireContext()).inflate(R.layout.widget_block, container, false);
         TextView textView = blockView.findViewById(R.id.text_view_block);
         Button editButton = blockView.findViewById(R.id.edit_button);
