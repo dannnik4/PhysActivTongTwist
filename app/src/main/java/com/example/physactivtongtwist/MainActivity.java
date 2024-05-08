05a6ae0a325c8e7188aa37ae331a1f8db3a37081
dannnik4 <daniilperstnev2001@gmail.com>
2024-05-08 23:51:54 +0300

    Updated MainActivity.java

--- app/src/main/java/com/example/physactivtongtwist/MainActivity.java
+++ app/src/main/java/com/example/physactivtongtwist/MainActivity.java
@@ -7,10 +7,12 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
