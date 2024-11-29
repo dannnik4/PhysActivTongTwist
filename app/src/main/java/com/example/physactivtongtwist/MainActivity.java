947dfa74cdcd1c4e7d72b8b15355cdb557fc0ecd
dannnik4 <daniilperstnev2001@gmail.com>
2024-11-29 14:25:30 +0200

    Updated MainActivity.java

--- app/src/main/java/com/example/physactivtongtwist/MainActivity.java
+++ app/src/main/java/com/example/physactivtongtwist/MainActivity.java
@@ -12,6 +12,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.media3.common.util.Log;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
