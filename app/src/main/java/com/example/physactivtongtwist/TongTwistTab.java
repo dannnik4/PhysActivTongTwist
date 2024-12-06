e42335426ca5fd6beddbe95336f91e50c9293645
dannnik4 <daniilperstnev2001@gmail.com>
2024-12-06 10:21:04 +0200

    Updated TongTwistTab.java

--- app/src/main/java/com/example/physactivtongtwist/TongTwistTab.java
+++ app/src/main/java/com/example/physactivtongtwist/TongTwistTab.java
@@ -26,7 +26,6 @@ public class TongTwistTab extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_layout, container, false);
-
         this.container = view.findViewById(R.id.container);
         loadWidgetText("TongTwistTab");
 
