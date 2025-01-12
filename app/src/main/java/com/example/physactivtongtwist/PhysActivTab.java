bfa7c696ebcdd0d066425e847860101faefa9041
dannnik4 <daniilperstnev2001@gmail.com>
2025-01-12 08:10:21 +0200

    Updated PhysActivTab.java

--- app/src/main/java/com/example/physactivtongtwist/PhysActivTab.java
+++ app/src/main/java/com/example/physactivtongtwist/PhysActivTab.java
@@ -1,7 +1,6 @@
 package com.example.physactivtongtwist;
 
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
