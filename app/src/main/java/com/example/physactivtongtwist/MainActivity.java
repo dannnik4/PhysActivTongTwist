1b1ae1137678477da8dcf2f5ffb8a2c9b2a7fc54
dannnik4 <daniilperstnev2001@gmail.com>
2025-02-08 07:09:01 +0200

    Updated MainActivity.java

--- app/src/main/java/com/example/physactivtongtwist/MainActivity.java
+++ app/src/main/java/com/example/physactivtongtwist/MainActivity.java
@@ -18,6 +18,8 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final Object R = ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
