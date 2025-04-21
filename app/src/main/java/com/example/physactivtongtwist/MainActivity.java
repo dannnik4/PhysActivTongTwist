8afeedb735006d723f8a6370f4c5fb433521f205
dannnik4 <daniilperstnev2001@gmail.com>
2025-04-21 08:11:46 +0300

    Updated MainActivity.java

--- app/src/main/java/com/example/physactivtongtwist/MainActivity.java
+++ app/src/main/java/com/example/physactivtongtwist/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.view.View;
 import android.widget.EditText;
